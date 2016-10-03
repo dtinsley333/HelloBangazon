897937b0dcf64223b51eba19a2e2c15007a70b2c
Denise tinsley <neecetinsley@live.com>
2016-10-03 13:38:48 -0500

    Created Bangazon Hello App

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+
+namespace HelloBangazon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CustomerGreeting().ToString());
+        }
+        public static string CustomerGreeting()
+        {
+            var purchaseDate=DateTime.Now;
+            var paymentDueDate=purchaseDate.AddDays(30);
+            var lastName="Smith"; 
+            var firstName="Bill";
+            var message= "your payment is due " + paymentDueDate.ToString("d");
+            var formalName=firstName + " " + lastName;
+
+            //Build the specific user message
+            var sb=new StringBuilder();
+            sb.Append("Hello ");
+            sb.Append(formalName);
+            sb.Append(" ");
+            sb.Append(message);
+            return sb.ToString();
+        }
+    }
+}
